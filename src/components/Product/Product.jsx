6040e6cd09e65b97288b0f538633f14d08d48d1b
heeeyooo <hokageow@icloud.com>
2024-09-30 18:56:58 +0300

    v1.3

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -7,98 +7,112 @@ import "./Product.css";
 
 // import required modules
 import { EffectCreative, Pagination, Navigation } from "swiper/modules";
-import { useEffect } from "react";
 
 const Product = ({ product }) => {
-  const {
-    id,
-    productContainer,
-    classProduct,
-    title,
-    info,
-    pack,
-    temp,
-    origin,
-    img,
-    classSwiper,
-    infoContainer,
-  } = product;
+    const {
+        id,
+        productContainer,
+        title,
+        info,
+        pack,
+        temp,
+        origin,
+        productImages,
+        classSwiper,
+        infoContainer,
+    } = product;
 
-  addEventListener("scroll", () => {
-    const slider = document.querySelectorAll(".my-swiper");
-    slider.forEach((slide) => {
-      if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
-        slide.classList.add("active");
-      }
+    addEventListener("scroll", () => {
+        const slider = document.querySelectorAll(".my-swiper");
+        slider.forEach((slide) => {
+            if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
+                slide.classList.add("active");
+            }
+        });
+        const productInfo = document.querySelectorAll(
+            ".product-info-container"
+        );
+        productInfo.forEach((product) => {
+            if (
+                window.innerHeight >
+                product.getBoundingClientRect().top + 200
+            ) {
+                product.classList.add("active");
+            }
+        });
+        const slider1 = document.querySelectorAll(".my-swiper-1");
+        slider1.forEach((slide) => {
+            if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
+                slide.classList.add("active");
+            }
+        });
+        const productInfo1 = document.querySelectorAll(
+            ".product-info-container-1"
+        );
+        productInfo1.forEach((product) => {
+            if (
+                window.innerHeight >
+                product.getBoundingClientRect().top + 200
+            ) {
+                product.classList.add("active");
+            }
+        });
     });
-    const productInfo = document.querySelectorAll(".product-info-container");
-    productInfo.forEach((product) => {
-      if (window.innerHeight > product.getBoundingClientRect().top + 200) {
-        product.classList.add("active");
-      }
-    });
-    const slider1 = document.querySelectorAll(".my-swiper-1");
-    slider1.forEach((slide) => {
-      if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
-        slide.classList.add("active");
-      }
-    });
-    const productInfo1 = document.querySelectorAll(".product-info-container-1");
-    productInfo1.forEach((product) => {
-      if (window.innerHeight > product.getBoundingClientRect().top + 200) {
-        product.classList.add("active");
-      }
-    });
-  });
 
-  return (
-    <>
-      <div id={id} className={productContainer}>
-        <div className={infoContainer}>
-          <a href={`#${id}`} className={`product-name ${classProduct}`}>
-            {title}
-          </a>
-          <p className="product-info">{info}</p>
-          <ul className="product-list-info">
-            <li className="pack">{pack}</li>
-            <li className="temp">{temp}</li>
-            <li className="origin">{origin}</li>
-          </ul>
-          <a className="call-us-btn" href="#section4">
-            CALL US
-          </a>
-        </div>
-        <Swiper
-          grabCursor={true}
-          effect={"creative"}
-          creativeEffect={{
-            prev: {
-              shadow: true,
-              translate: [0, 0, -400],
-            },
-            next: {
-              translate: ["100%", 0, 0],
-            },
-          }}
-          modules={[EffectCreative, Pagination, Navigation]}
-          navigation={true}
-          pagination={{
-            clickable: true,
-          }}
-          //
-          observer={true}
-          observeParents={true}
-          //
-          className={classSwiper}
-        >
-          <SwiperSlide>
-            <img src={`./img/products/${img}`} alt={title} />
-          </SwiperSlide>
-          <SwiperSlide>More photos coming soon!</SwiperSlide>
-        </Swiper>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div id={id} className={productContainer}>
+                <div className={infoContainer}>
+                    <a href={`#${id}`} className={`product-name`}>
+                        {title}
+                    </a>
+                    <p className="product-info">{info}</p>
+                    <ul className="product-list-info">
+                        <li className="pack">{pack}</li>
+                        <li className="temp">{temp}</li>
+                        <li className="origin">{origin}</li>
+                    </ul>
+                    <a className="call-us-btn" href="#section4">
+                        CALL US
+                    </a>
+                </div>
+                <Swiper
+                    grabCursor={true}
+                    effect={"creative"}
+                    creativeEffect={{
+                        prev: {
+                            shadow: true,
+                            translate: [0, 0, -400],
+                        },
+                        next: {
+                            translate: ["100%", 0, 0],
+                        },
+                    }}
+                    modules={[EffectCreative, Pagination, Navigation]}
+                    navigation={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    //
+                    observer={true}
+                    observeParents={true}
+                    //
+                    className={classSwiper}
+                >
+                    {productImages.map((productImage, index) => {
+                        return (
+                            <SwiperSlide key={index}>
+                                <img
+                                    src={`./img/products/${productImage}`}
+                                    alt={title}
+                                />
+                            </SwiperSlide>
+                        );
+                    })}
+                </Swiper>
+            </div>
+        </>
+    );
 };
 
 export default Product;
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,63 +1,70 @@
 import { useState } from "react";
 import Product from "../../components/Product/Product";
-import { productsData } from "../../data/DataProducts";
+import { productsData } from "../../data/productsData";
 import "./Products.css";
 
 const Products = () => {
-  const [search, setSearch] = useState("");
+    const [search, setSearch] = useState("");
 
-  addEventListener("keyup", () => {
-    const slider = document.querySelectorAll(".my-swiper");
-    slider.forEach((slide) => {
-      // if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
-      slide.classList.add("active");
-      // }
+    addEventListener("keyup", () => {
+        const slider = document.querySelectorAll(".my-swiper");
+        slider.forEach((slide) => {
+            // if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
+            slide.classList.add("active");
+            // }
+        });
+        const productInfo = document.querySelectorAll(
+            ".product-info-container"
+        );
+        productInfo.forEach((product) => {
+            // if (window.innerHeight > product.getBoundingClientRect().top + 200) {
+            product.classList.add("active");
+            // }
+        });
+        const slider1 = document.querySelectorAll(".my-swiper-1");
+        slider1.forEach((slide) => {
+            // if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
+            slide.classList.add("active");
+            // }
+        });
+        const productInfo1 = document.querySelectorAll(
+            ".product-info-container-1"
+        );
+        productInfo1.forEach((product) => {
+            // if (window.innerHeight > product.getBoundingClientRect().top + 200) {
+            product.classList.add("active");
+            // }
+        });
     });
-    const productInfo = document.querySelectorAll(".product-info-container");
-    productInfo.forEach((product) => {
-      // if (window.innerHeight > product.getBoundingClientRect().top + 200) {
-      product.classList.add("active");
-      // }
-    });
-    const slider1 = document.querySelectorAll(".my-swiper-1");
-    slider1.forEach((slide) => {
-      // if (window.innerHeight > slide.getBoundingClientRect().top + 200) {
-      slide.classList.add("active");
-      // }
-    });
-    const productInfo1 = document.querySelectorAll(".product-info-container-1");
-    productInfo1.forEach((product) => {
-      // if (window.innerHeight > product.getBoundingClientRect().top + 200) {
-      product.classList.add("active");
-      // }
-    });
-  });
-  return (
-    <div className="products-container" id="section3">
-      <h3 className="products-main-info">Our products</h3>
-      <p className="products-info">
-        We specialise in freshly frozen bilberry, lingonberry, plum, strawberry,
-        sourcherry, rosehip and elderberry. All our products are grown and
-        harvested exclusively in ecologically clean areas of Ukraine
-      </p>
-      <p className="filter-title">Filter</p>
-      <input
-        className="search-input"
-        type="text"
-        placeholder="Product"
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      {productsData
-        .filter((item) => {
-          return search.toLowerCase() === ""
-            ? item
-            : item.title.toLowerCase().startsWith(search.toLowerCase());
-        })
-        .map((product) => {
-          return <Product product={product} key={product.id} />;
-        })}
-    </div>
-  );
+    return (
+        <div className="products-container" id="section3">
+            <h3 className="products-main-info">Our products</h3>
+            <p className="products-info">
+                We specialise in freshly frozen bilberry, lingonberry, plum,
+                strawberry, sourcherry, rosehip and elderberry. All our products
+                are grown and harvested exclusively in ecologically clean areas
+                of Ukraine
+            </p>
+            <p className="filter-title">Filter</p>
+            <input
+                className="search-input"
+                type="text"
+                placeholder="Product"
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            {productsData
+                .filter((item) => {
+                    return search.toLowerCase() === ""
+                        ? item
+                        : item.title
+                              .toLowerCase()
+                              .startsWith(search.toLowerCase());
+                })
+                .map((product) => {
+                    return <Product product={product} key={product.id} />;
+                })}
+        </div>
+    );
 };
 
 export default Products;
